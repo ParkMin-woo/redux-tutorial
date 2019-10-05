a80ed39d9223c8ec6b107c37d2819a91200c94a7
Park Minwoo <lwmwpark3523@gmail.com>
2019-10-05 18:25:26 +0900

    redux-tutorial commit <Review>할 것!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from "react-redux";
+import {updateUser} from "./redux/actions";
 
-function App() {
+function App(props) {
+  console.log("props in App : " , props);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Demo_REDUX</p>
+      <p>{props.user}</p>
+      <button onClick={() => props.updateUser("KIM SOOYOUNG")}>액션디스패치</button>
     </div>
   );
 }
 
-export default App;
+// store의 state를 props로 mapping해야함.
+const mapStateToProps = (state) => (
+  // 왼쪽은 props, 오른쪽은 store의 state
+  {
+    products : state.products,
+    user : state.user,
+  }
+);
+
+// action을 dispatch해주는 function이 props를 매핑
+const mapActionToProps = (dispatch) => ({
+  // 왼쪽은 props, 오른쪽은 action을 dispatch해주는 function
+  updateUser : (name) => dispatch(updateUser(name))
+});
+
+// export default App;
+
+// 커링펑션, HoC
+export default connect(mapStateToProps , mapActionToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,57 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {combineReducers, createStore} from "redux";
+import {Provider} from "react-redux";
+import {productReducer} from "./redux/reducers/product";
+import {userReducer} from "./redux/reducers/user";
+import {allReducers} from "./redux/reducers";
+import {store} from "./redux/store";
+import {updateUser} from "./redux/actions";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// pure function
+// const reducer = (state , action) => {
+// 	if(action.type === 'changeState') {
+// 		return action.payload.newState;
+// 	}
+// 	return 'State';
+// }
+
+// default parameter 문법 사용
+
+
+
+
+
+
+
+// store.getState()의 결과는 잘 기억해 둡시다!
+
+console.log("store in index : " , store);
+console.log("store.getState() in index : " , store.getState());
+
+
+
+// store.dispatch(updateUser);
+
+console.log("store.getState() in index : " , store.getState());
+// {
+// 	products : [],
+// 		user : 'KIM SOOYOUNG',
+// }
+
+// const action = {
+// 	type : 'changeState' ,
+// 	payload : {
+// 		newState : 'New State',
+// 	}
+// }
+
+// store.subscribe(() => console.log('subscribe : ' , store.getState()));
+// store.dispatch(action);
+// console.log("store.getState() in index : " , store.getState());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
